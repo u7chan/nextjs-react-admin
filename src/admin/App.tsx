488cc3a206027aea60aecc1bf15af8c63a9e3461
una <unagamix@gmail.com>
2023-02-14 23:22:59 +0900

    clean

--- src/admin/App.tsx
+++ src/admin/App.tsx
@@ -5,22 +5,22 @@ import jsonServerProvider from 'ra-data-json-server'
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 
 const PostList = (props: any) => {
-    return (
-        <List {...props}>
-            <Datagrid>
-                <TextField source='id' />
-                <TextField source='title' />
-                <TextField source='body' />
-            </Datagrid>
-        </List>
-    )
+  return (
+    <List {...props}>
+      <Datagrid>
+        <TextField source='id' />
+        <TextField source='title' />
+        <TextField source='body' />
+      </Datagrid>
+    </List>
+  )
 }
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name='posts' list={PostList} />
-        {/* <Resource name="comments" list={ListGuesser} /> */}
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource name='posts' list={PostList} />
+    {/* <Resource name="comments" list={ListGuesser} /> */}
+  </Admin>
 )
 
 export default App
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ import dynamic from 'next/dynamic'
 const App = dynamic(() => import('../admin/App'), { ssr: false })
 
 const Home = () => {
-    return <App />
+  return <App />
 }
 export default Home
